bc95ade51a8a59505b3b1d447f2b792ec8635373
TanjereeN98 <ahmadcyc35@gmail.com>
2024-08-07 04:59:23 +0300

    Fixing task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    var innerTask = true;  // Renamed variable to avoid overwriting
-    var innerTask2 = false;  // Renamed variable to avoid overwriting
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
   }
 
-  // The outer variables `task` and `task2` are unaffected by the block scope changes
   return [task, task2];
 }
